045d8630552202f4d25733a6d9b410eb5cfeceb0
Yanlin Gong <nielgong@gmail.com>
2019-08-30 22:46:17 +1000

    init code

--- src/main.js
+++ src/main.js
@@ -2,8 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 解决有些机型的click事件300ms延时
+import fastClick from 'fastclick'
+// 解决css的reset问题, 保证每个机型的样式统一
+import 'normalize.css'
+// 解决大分辨下的边框问题
+import './assets/style/border.css'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 new Vue({
   router,
